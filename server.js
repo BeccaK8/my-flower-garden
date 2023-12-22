b42c0cf9cf1343ab3892fc9c8c2ce4871d111a6e
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2023-12-22 08:58:20 -0600

    inital app setup and connect

---
+++ server.js
@@ -0,0 +1,42 @@
+/*******************************************/
+/*****       Import Dependencies       *****/
+/*******************************************/
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+
+/*******************************************/
+/*****          Import Routers         *****/
+/*******************************************/
+
+/*******************************************/
+/***** Create app + set up view engine *****/
+/*******************************************/
+const app = express();
+
+// view engine - ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/*******************************************/
+/*****           Middleware            *****/
+/*******************************************/
+
+
+/*******************************************/
+/*****              Routes             *****/
+/*******************************************/
+// basic home route
+app.get('/', (req, res) => {
+    //const { username, loggedIn, userId } = req.session;
+    res.send('the app is connected');
+});
+
+/*******************************************/
+/*****         Server Listener         *****/
+/*******************************************/
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log('Your server is running...better go catch it');
+});
