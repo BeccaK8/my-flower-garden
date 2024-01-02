527c685804f358125969376cddb0cadf41ffd72e
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-02 10:15:12 -0600

    delete container

--- controllers/containerControllers.js
+++ controllers/containerControllers.js
@@ -72,6 +72,41 @@ router.get('/containers/:id/delete', (req, res) => {
         });
 });
 
+// DELETE -> /containers/:id
+// Delete container
+// Only available to authorized users
+router.delete('/containers/:id', (req, res) => {
+    const { username, loggedIn, userId } = req.session;
+
+    // Target specific container
+    const containerId = req.params.id;
+
+    // Find the garden with that container Id in the database
+    Garden.findOne( {'sections.containers._id' : containerId})
+        .then(foundGarden => {
+            // If we couldn't find the garden, just redirect to the My Gardens index page
+            if (!foundGarden) return res.redirect('/gardens');
+
+            // Determine if logged in user is authorized to update it (that is, are they the owner of the garden)
+            if (foundGarden.owner == userId) {
+                // If authorized, remove the container subdoc from the correct section sub doc 
+                findContainer: for (let i = 0; i < foundGarden.sections.length; i++) {
+                    foundGarden.sections[i].containers.remove(containerId);
+                }
+                
+                // Save the garden
+                return foundGarden.save();
+            } else {
+                // If not authorized, redirect to error page
+                throw new Error('You are Not Authorized to Update this Garden.');
+            }
+        })
+        .then(returnedGarden => {
+            // Redirect to My Gardens
+            res.redirect(`/gardens/${returnedGarden.id}`);
+        })
+});
+
 // POST /sections/:id/containers
 // Create new container and redirect to show section paage
 router.post('/sections/:id/containers', (req, res) => {
